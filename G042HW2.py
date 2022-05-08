5a5806610fadab0acb7c7c11ebc70e72f7dc728d
Morippi <Morippi>
2022-05-08 16:26:10 +0200

    Everything done: TODO check all results, try to improve efficiency in balls

--- G042HW2.py
+++ G042HW2.py
@@ -1,6 +1,6 @@
 import sys
 import math
-import copy
+from time import perf_counter
 
 def readVectorsSeq(filename):
     with open(filename) as f:
@@ -32,46 +32,66 @@ def minDistance(P, n):
 
 def pointsInRadius(P, w, x, r):
     # returns set of points in radius r from x
-    ball = []
+    ball_weight = 0
     # print("x: ", x)
-    for i in range(len(P)):
+    for point in P:
         # print(P[i])
-        if euclidean(P[i], x) < r:
+        if euclidean(point, x) < r:
             # print(euclidean(P[i], x), " is in radius")
-            ball.append(w[i])
-    return sum(ball)
+            ball_weight += P[point]
+    return ball_weight
 
 
 def SeqWeightedOutliers(inputPoints, weights, k, z, alpha=0):
     # r <- (Min distance between first k+z+1 points) / 2
     r = minDistance(inputPoints, k + z + 1)
-    num_iter = 0
+
+    bad1 = 0
+    bad2 = 0
+    bad3 = 0
+    num_iter = 1
     while True:
-        Z = copy.deepcopy(inputPoints)
+        Z = {}
+        for i in range(len(inputPoints)):
+            Z[inputPoints[i]] = weights[i]
         S = []
         W_z = sum(weights)
         while len(S) < k and W_z > 0:
             MAX = 0
             new_center = tuple()
+
+            start_time = perf_counter()
             for x in Z:
                 # ball weight is the sum of weights of all point in the radius (1+2*alpha)r
-                ball_weight = pointsInRadius(inputPoints, weights, x, (1 + 2 * alpha) * r)
+                ball_weight = pointsInRadius(Z, weights, x, (1 + 2 * alpha) * r)
                 if ball_weight > MAX:
                     MAX = ball_weight
                     new_center = x
             S.append(new_center)
-            print(S)
+            end_time = perf_counter()
+            bad1 += end_time - start_time
+            start_time = perf_counter()
+            points_to_remove = []
             for y in Z:
                 if euclidean(y, new_center) < (3 + 4 * alpha) * r:
-                    W_z -= 1
-                    Z.remove(y)
+                    W_z -= Z[y]
+                    points_to_remove.append(y)
+            end_time = perf_counter()
+            bad2 += end_time - start_time
+            start_time = perf_counter()
+            for point_to_remove in points_to_remove:
+                del Z[point_to_remove]
+            end_time = perf_counter()
+            bad3 += end_time - start_time
         if W_z <= z:
             break
         else:
             r = 2 * r
             num_iter += 1
-
-    print("S: ", S)
+        print(num_iter, W_z, z)
+    print(bad1)
+    print(bad2)
+    print(bad3)
     return S, r, num_iter
 
 
@@ -81,16 +101,15 @@ def ComputeObjective(P, S, z):
     # - the distance to the closest center
     # - the center
     distances = []
-    total_distance = 0
     for point in P:
-        distance = 0
+        min_distance = float('inf')
         closest_center = None
         for center in S:
-            distance = euclidian(point, center)
+            distance = euclidean(point, center)
             if min_distance > distance:
                 min_distance = distance
                 closest_center = center
-        distances.append((min_distances, closest_center))
+        distances.append((min_distance, closest_center))
 
     # We sort the list on the distances
     distances = sorted(distances, key=lambda couple: couple[0])
@@ -110,8 +129,8 @@ def ComputeObjective(P, S, z):
     # The sum of the maximum distances between the center of a cluster and the point
     # in the same cluster is the objective value
     objective_value = 0
-    for center in max_distances.keys:
-        objective_value += max_distances[center]
+    for center in max_distances:
+        objective_value = max(objective_value,max_distances[center])
 
     return objective_value
 
@@ -131,14 +150,18 @@ if __name__ == '__main__':
     z = int(z)
 
     r_init = minDistance(inputPoints, k + z + 1)
+    start_time = perf_counter()
     S, r_fin, num_iter = SeqWeightedOutliers(inputPoints, weights, k, z)
+    end_time = perf_counter()
+    time = int((end_time - start_time)*1000)
 
+    objective = ComputeObjective(inputPoints, S, z)
     # OUTPUT
-    print("Input size n =  ", len(inputPoints))
-    print("Number of centers k =  ", k)
-    print("Number of ouliers z =  ", z)
-    print("Initial guess =  ", r_init)
-    print("Final guess =  ", r_fin)
-    print("Number of guesses =  ", num_iter)
-    print("Objective function =  ")
-    print("Time of SeqWeightedOutliers = ")
+    print("Input size n = ", len(inputPoints))
+    print("Number of centers k = ", k)
+    print("Number of outliers z = ", z)
+    print("Initial guess = ", r_init)
+    print("Final guess = ", r_fin)
+    print("Number of guesses = ", num_iter)
+    print("Objective function = ", objective)
+    print("Time of SeqWeightedOutliers = ", time)
